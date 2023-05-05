f98c54d53bade8299c702fe10ce4242d9125c23a
Abdalla Elabd <abdalla.mohamed.elabd@gmail.com>
2023-05-05 23:36:13 +0200

    revert to using npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
