753c8482681c1e0da7b577188a04cd7da78b9ce1
Abdalla Elabd <abdalla.mohamed.elabd@gmail.com>
2023-05-05 22:18:11 +0200

    refactor chip in tailwind

--- src/components/Chip/Chip.stories.tsx
+++ src/components/Chip/Chip.stories.tsx
@@ -1,15 +1,31 @@
+import type { Meta } from '@storybook/react';
+
 import { Chip } from './Chip';
 
-export default {
+const meta: Meta<typeof Chip> = {
+  title: 'Chip',
+  tags: ['autodocs'],
   component: Chip,
 };
 
-export function Default() {
+export default meta;
+
+export function Variants() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+    <div className="flex flex-col gap-2">
       <Chip variant="primary">Primary</Chip>
       <Chip variant="secondary">Secondary</Chip>
       <Chip variant="neutral">Neutral</Chip>
+    </div>
+  );
+}
+
+export function Sizes() {
+  return (
+    <div className="flex flex-col gap-2">
+      <Chip size="lg">Large</Chip>
+      <Chip size="md">Medium</Chip>
+      <Chip size="sm">Small</Chip>
     </div>
   );
 }
--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,42 +1,28 @@
-import { theme } from '@styles/theme';
 import { PropsWithChildren } from 'react';
-import styled, { css } from 'styled-components';
+import { tv, VariantProps } from 'tailwind-variants';
 
-import { Text } from '../Text';
+const chip = tv({
+  base: 'flex w-fit items-center rounded-full px-2 py-1 text-xs font-light text-slate-50',
+  variants: {
+    variant: {
+      primary: 'bg-blue-600',
+      secondary: 'bg-yellow-600',
+      neutral: 'bg-slate-500',
+    },
+    size: {
+      sm: 'text-xs',
+      md: 'text-sm',
+      lg: 'text-base',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+});
 
-type ChipVariant = 'primary' | 'secondary' | 'neutral';
+type ChipProps = PropsWithChildren<VariantProps<typeof chip>>;
 
-const getColorsFromVariant = (variant: ChipVariant) => {
-  const colors = {
-    primary: theme.colors.primaryDark,
-    secondary: theme.colors.secondaryDark,
-    neutral: theme.colors.neutral,
-  }[variant];
-  return css`
-    background: ${colors.background};
-    color: ${colors.foreground};
-  `;
-};
-
-const Container = styled.div<{ variant: ChipVariant }>`
-  display: flex;
-  align-items: center;
-  border-radius: 0.75rem;
-  padding: 0.25rem 0.5rem;
-  ${({ variant }) => getColorsFromVariant(variant)};
-`;
-
-interface ChipProps {
-  variant?: ChipVariant;
-}
-
-export function Chip({
-  children,
-  variant = 'secondary',
-}: PropsWithChildren<ChipProps>) {
-  return (
-    <Container variant={variant}>
-      <Text variant="caption">{children}</Text>
-    </Container>
-  );
+export function Chip({ children, ...variantProps }: ChipProps) {
+  return <div className={chip(variantProps)}>{children}</div>;
 }
