e78fd409c1ed0fe8c48541c5f90bceba72426f75
Abdalla Elabd <abdalla.mohamed.elabd@gmail.com>
2023-05-06 08:27:34 +0200

    rename file 1

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import './index.css';
 
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './App2';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
