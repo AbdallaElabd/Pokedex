e81f63fbfece5bef0884940050a444717c75fec6
Abdalla Elabd <abdalla.mohamed.elabd@gmail.com>
2023-05-15 12:19:10 +0300

    update logo

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -1,18 +1,18 @@
 import { Link } from '@tanstack/router';
 import { PropsWithChildren } from 'react';
 
-import logo from './header-logo.png';
+import logo from './pokedex.png';
 
 export function Page({ children }: PropsWithChildren) {
   return (
     <div className="relative flex h-full flex-col">
       <Link
         to="/"
-        className="flex justify-center bg-gradient-to-bl from-blue-700 to-blue-800 p-4 sm:justify-start md:p-8"
+        className="flex justify-center bg-gradient-to-bl from-blue-700 to-blue-800 p-4 sm:justify-start md:px-12 md:py-6"
       >
-        <div className="flex -translate-x-10 items-center gap-2 sm:translate-x-0 sm:gap-4">
+        <div className="flex -translate-x-10 items-center gap-4 sm:translate-x-0 sm:gap-8">
           <img src={logo} alt="Pokédex" className="h-full w-36" />
-          <h1 className="text-4xl font-extralight text-white sm:text-5xl">
+          <h1 className="letter text-4xl font-light text-white sm:text-5xl">
             Pokédex
           </h1>
         </div>
